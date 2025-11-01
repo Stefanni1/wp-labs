ae0bde46f8e8cad77c3153c39367d3782f470a53
Stefi Lalkova <lalkovastefi29@gmail.com>
2025-11-01 22:29:00 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp/lab/WpLabsApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpLabsApplication.class, args);
+	}
+
+}
